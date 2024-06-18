b7e622d3da403f1fb3af292090d8cfc20319daa5
Glenn Phillips <gjp214@gmail.com>
2024-06-18 15:26:26 -0400

    Update application with latest changes

---
+++ run.ps1
@@ -0,0 +1,24 @@
+
+# PowerShell script to look for updates on the GitHub repository, update the application, and run the program.
+
+# Variables
+$repoDir = "C:\CiscoSwitchPortApp"
+$repoUrl = "https://github.com/gp3264/CiscoSwitchPortApp.git"
+
+# Navigate to the repository directory
+if (-not (Test-Path $repoDir)) {
+    Write-Error "Repository directory not found."
+    exit 1
+}
+cd $repoDir
+
+
+# Set up the Python virtual environment
+& .\venv\Scripts\Activate.ps1
+
+
+# Run the Flask application
+$env:FLASK_APP = "app"
+python run.py
+
+Read-Host -Prompt "Press Enter to continue..."
