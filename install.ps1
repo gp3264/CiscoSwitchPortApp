e57c4333cfe7bbb2982ed994c097def281c3b643
Glenn Phillips <gjp214@gmail.com>
2024-06-21 11:27:30 -0400

    Update application with latest changes

--- install.ps1
+++ install.ps1
@@ -4,18 +4,19 @@
 # Variables
 $pythonInstallerUrl = "hhttps://www.python.org/ftp/python/3.12.4/python-3.12.4-amd64.exe"
 $pythonInstallerPath = "$env:TEMP\python-installer.exe"
+#https://docs.python.org/3/using/windows.html#installing-without-ui
 $repoUrl = "https://github.com/gp3264/CiscoSwitchPortApp.git"
 $installDir = "C:\CiscoSwitchPortApp"
 
 # Check if Python is installed
-$pythonInstalled = Get-Command python -ErrorAction SilentlyContinue
+$pythonInstalled = Get-Command C:\Python\Python312\python.exe -ErrorAction SilentlyContinue
 if (-not $pythonInstalled) {
     # Download and install Python
     Invoke-WebRequest -Uri $pythonInstallerUrl -OutFile $pythonInstallerPath
-    Start-Process -FilePath $pythonInstallerPath -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
+    Start-Process -FilePath $pythonInstallerPath -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1 DefaultAllUsersTargetDir=C:\Python\Python312" -Wait
 
     # Verify Python installation
-    $pythonInstalled = Get-Command python -ErrorAction SilentlyContinue
+    $pythonInstalled = Get-Command C:\Python\Python312\python.exe -ErrorAction SilentlyContinue
     if (-not $pythonInstalled) {
         Write-Error "Python installation failed."
         exit 1
@@ -51,12 +52,19 @@ if (Test-Path $installDir) {
 }
 git clone $repoUrl $installDir
 
+
 # Set up the Python virtual environment and install dependencies
 cd $installDir
 python -m venv venv
 & .\venv\Scripts\Activate.ps1
 & .\venv\Scripts\python.exe -m pip install --upgrade pip
 & .\venv\Scripts\pip.exe install -r requirements.txt
+
+# Copy Nexpose API Client from resources AFter VNV
+if (Test-Path $installDir+"\venv\Lib\site-packages") {
+    Write-Host Copying Rapid7 Nexpose API to Library
+    copy $installDir+"\resources\rapid7vmconsole" $installDir+".\venv\Lib\site-packages"
+}
 
 
 Read-Host -Prompt "Press Enter to continue..."
